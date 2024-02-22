1fe00691621c100d819b7eee3d5b9ae6e587ece9
vinomaya2024 <vidyavinochan2015@gmail.com>
2024-02-22 12:47:07 +0530

    new

---
+++ operators.py
@@ -0,0 +1,32 @@
+#Arithmetic Opertors
+'''x=10
+y=2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x//y)
+'''
+
+# Assignment Operators
+x=10
+x+=10
+print(x)
+x=10
+x-=10
+print(x)
+
+x=10
+x*=10
+print(x)
+
+x=10
+x=10
+print(x)
+x=10
+x+=10
+print(x)
+x=10
+x+=10
+print(x)
